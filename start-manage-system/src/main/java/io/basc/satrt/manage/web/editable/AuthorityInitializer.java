d126b50ea30cdf31f13b9f4a2dd4a18100154deb
舒超文 <shuchaowen@zx62580.com>
2021-09-29 15:17:34 +0800

    cleanup

--- start-manage-system/src/main/java/io/basc/satrt/manage/web/editable/AuthorityInitializer.java
+++ start-manage-system/src/main/java/io/basc/satrt/manage/web/editable/AuthorityInitializer.java
@@ -1,56 +1,36 @@
 package io.basc.satrt.manage.web.editable;
 
+import java.util.List;
+
 import io.basc.framework.boot.ApplicationPostProcessor;
 import io.basc.framework.boot.ConfigurableApplication;
 import io.basc.framework.context.annotation.Provider;
-import io.basc.framework.logger.Logger;
-import io.basc.framework.logger.LoggerFactory;
-
-import java.util.List;
 
 @Provider
 public class AuthorityInitializer implements ApplicationPostProcessor {
-	private static Logger logger = LoggerFactory
-			.getLogger(AuthorityInitializer.class);
 
 	@Override
 	public void postProcessApplication(ConfigurableApplication application)
 			throws Throwable {
-		if (!(canInit(application, EditorRegistry.class) || canInit(
-				application, EditorResolver.class))) {
-			return;
-		}
+		if(application.isInstance(EditorRegistry.class) && application.isInstance(EditorResolver.class)) {
+			EditorRegistry editorRegistry = application.getBeanFactory()
+					.getInstance(EditorRegistry.class);
+			EditorResolver editorResolver = application.getBeanFactory()
+					.getInstance(EditorResolver.class);
+			for (Class<?> clazz : application.getContextClasses()) {
+				if (!editorResolver.canResolve(clazz)) {
+					continue;
+				}
 
-		EditorRegistry editorRegistry = application.getBeanFactory()
-				.getInstance(EditorRegistry.class);
-		EditorResolver editorResolver = application.getBeanFactory()
-				.getInstance(EditorResolver.class);
-		for (Class<?> clazz : application.getContextClasses()) {
-			if (!editorResolver.canResolve(clazz)) {
-				continue;
-			}
+				List<Editor> editors = editorResolver.resolve(clazz);
+				if (editors == null) {
+					continue;
+				}
 
-			List<Editor> editors = editorResolver.resolve(clazz);
-			if (editors == null) {
-				continue;
-			}
-
-			for (Editor editor : editors) {
-				editorRegistry.register(editor);
+				for (Editor editor : editors) {
+					editorRegistry.register(editor);
+				}
 			}
 		}
-	}
-
-	private boolean canInit(ConfigurableApplication application, Class<?> clazz) {
-		if (!application.getBeanFactory().isInstance(clazz)) {
-			logger.error("not instance {}", clazz);
-			return false;
-		}
-
-		if (!application.getBeanFactory().isSingleton(clazz)) {
-			logger.error("{} not is singleton", clazz);
-			return false;
-		}
-		return true;
 	}
 }

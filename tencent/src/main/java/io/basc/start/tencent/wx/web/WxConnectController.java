76963f5d0be098e9897bf3d0b8e938c821155038
wcnnkh <247044007@qq.com>
2021-10-16 18:46:17 +0800

    cleanup

--- tencent/src/main/java/io/basc/start/tencent/wx/web/WxConnectController.java
+++ tencent/src/main/java/io/basc/start/tencent/wx/web/WxConnectController.java
@@ -1,16 +1,5 @@
 package io.basc.start.tencent.wx.web;
 
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.validation.constraints.NotEmpty;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
-
 import io.basc.framework.beans.annotation.Value;
 import io.basc.framework.beans.ioc.value.ResourceValueProcessor;
 import io.basc.framework.codec.support.URLCodec;
@@ -24,6 +13,18 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.constraints.NotEmpty;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 /**
  * 辅助类
@@ -41,6 +42,7 @@ public class WxConnectController {
 	private String html;
 
 	@Path("/call/{url}")
+	@GET
 	public String call(@PathParam("url") String url, String code, String state,
 			@Parameter(hidden = true) ServerHttpRequest request,
 			@Parameter(hidden = true) ServerHttpResponse response) {
@@ -51,19 +53,23 @@ public class WxConnectController {
 		String redirectUri = UriUtils.appendQueryParams(url, map, new URLCodec(request.getCharacterEncoding()));
 		return html.replace("#{url}", redirectUri);
 	}
+	
+	private String toCallUrl(ServerHttpRequest request, String host, String redirectUri) throws UnsupportedEncodingException{
+		return host + PATH + "/call/" + URLEncoder.encode(URLEncoder.encode(redirectUri, request.getCharacterEncoding()), request.getCharacterEncoding());
+	}
 
 	@Operation(description = "授权登录")
 	@Path("/oauth2/authorize")
 	@GET
 	@ApiResponse(responseCode = "302")
-	public void oauth2Authorize(@Parameter(hidden = true) ServerHttpRequest serverRequest,
+	public void oauth2Authorize(@Parameter(hidden = true) ServerHttpRequest request,
 			@Parameter(hidden = true) ServerHttpResponse serverResponse,
 			@Parameter(description = "服务主机的地址", required = true) @QueryParam("host") @NotEmpty String host,
 			@Parameter(description = "微信appid", required = true) @QueryParam("appid") @NotEmpty String appid,
 			@Parameter(description = "授权方式", example = "snsapi_base") @QueryParam("scope") @NotEmpty String scope,
 			@Parameter(description = "回调地址", required = true) @QueryParam("redirectUri") @NotEmpty String redirectUri,
 			@Parameter(description = "回调参数") @QueryParam("state") String state) throws IOException {
-		String callUrl = host + PATH + "/call" + URLEncoder.encode(redirectUri, serverRequest.getCharacterEncoding());
+		String callUrl = toCallUrl(request, host, redirectUri);
 		String url = WeiXinUtils.authorizeUlr(appid, callUrl, scope, state);
 		serverResponse.sendRedirect(url);
 	}
@@ -72,14 +78,14 @@ public class WxConnectController {
 	@Path("/qrconnect")
 	@GET
 	@ApiResponse(responseCode = "302")
-	public void qrconnect(@Parameter(hidden = true) ServerHttpRequest serverRequest,
+	public void qrconnect(@Parameter(hidden = true) ServerHttpRequest request,
 			@Parameter(hidden = true) ServerHttpResponse serverResponse,
 			@Parameter(description = "服务主机的地址", required = true) @QueryParam("host") @NotEmpty String host,
 			@Parameter(description = "微信appid", required = true) @QueryParam("appid") @NotEmpty String appid,
 			@Parameter(description = "授权方式", required = true, example = "snsapi_login") @QueryParam("scope") @NotEmpty String scope,
 			@Parameter(description = "回调地址", required = true) @QueryParam("redirectUri") @NotEmpty String redirectUri,
 			@Parameter(description = "回调参数") @QueryParam("state") String state) throws IOException {
-		String callUrl = host + PATH + "/call" + URLEncoder.encode(redirectUri, serverRequest.getCharacterEncoding());
+		String callUrl = toCallUrl(request, host, redirectUri);
 		String url = WeiXinUtils.qrcodeAuthorizeUrl(appid, callUrl, scope, state);
 		serverResponse.sendRedirect(url);
 	}

a26af4725e6b181bf11b1cd3fef6b3df2fe6ce12
wcnnkh <247044007@qq.com>
2021-10-19 21:58:36 +0800

    cleanup

---
+++ start-app/src/main/java/io/basc/start/app/configure/CorsAutoConfigure.java
@@ -0,0 +1,36 @@
+package io.basc.start.app.configure;
+
+import io.basc.framework.boot.ApplicationPostProcessor;
+import io.basc.framework.boot.ConfigurableApplication;
+import io.basc.framework.context.annotation.Provider;
+import io.basc.framework.core.Ordered;
+import io.basc.framework.util.StringUtils;
+import io.basc.framework.web.cors.Cors;
+import io.basc.framework.web.cors.CorsRegistry;
+import io.basc.start.app.configure.AppConfigure;
+
+/**
+ * 默认的cors自动配置
+ * 
+ * @author shuchaowen
+ *
+ */
+@Provider(order = Ordered.LOWEST_PRECEDENCE)
+public class CorsAutoConfigure implements ApplicationPostProcessor {
+
+	@Override
+	public void postProcessApplication(ConfigurableApplication application)
+			throws Throwable {
+		// 判断是否禁用
+		if (!application.getEnvironment().getBooleanValue(
+				"io.basc.start.app.admin.cors.disable")) {
+			AppConfigure appConfigure = application
+					.getInstance(AppConfigure.class);
+			CorsRegistry corsRegistry = application
+					.getInstance(CorsRegistry.class);
+			corsRegistry.add(
+					StringUtils.cleanPath(appConfigure.getAdminController()
+							+ "/**"), Cors.DEFAULT);
+		}
+	}
+}

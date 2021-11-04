6c50160400baf1f57da4d71419ce9e62228cecf6
舒超文 <shuchaowen@zx62580.com>
2021-11-04 13:34:53 +0800

    cleanup

--- start-data-db/src/main/java/io/basc/start/data/db/DbDataService.java
+++ start-data-db/src/main/java/io/basc/start/data/db/DbDataService.java
@@ -1,7 +1,5 @@
 package io.basc.start.data.db;
 
-import io.basc.framework.beans.BeanFactory;
-import io.basc.framework.beans.BeanFactoryAware;
 import io.basc.framework.context.annotation.Provider;
 import io.basc.framework.db.DB;
 import io.basc.start.data.DataManager;
@@ -9,18 +7,13 @@ import io.basc.start.data.DefaultDataService;
 import io.basc.start.data.annotation.Editable;
 
 @Provider
-public class DbDataService extends DefaultDataService implements BeanFactoryAware{
+public class DbDataService extends DefaultDataService{
 	private final DB db;
 
 	public DbDataService(DB db) {
 		this.db = db;
 	}
 	
-	@Override
-	public void setBeanFactory(BeanFactory beanFactory) {
-		configure(beanFactory);
-	}
-
 	public boolean isEditable(Class<?> entityClass) {
 		return entityClass.isAnnotationPresent(Editable.class);
 	}
